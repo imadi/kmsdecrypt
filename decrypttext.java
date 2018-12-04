3ae4b6dab1ff23af7ada468a55df0794723f6f02
Adithya Krishna <adithyakrishna.k@gmail.com>
2018-12-04 08:50:04 +0530

    Create decrypttext.java

---
+++ decrypttext.java
@@ -0,0 +1,31 @@
+package com.adi.testawssdk;
+
+import com.amazonaws.services.kms.AWSKMS;
+import com.amazonaws.services.kms.AWSKMSClientBuilder;
+import com.amazonaws.services.kms.model.DecryptRequest;
+import com.amazonaws.services.kms.model.DecryptResult;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) throws IOException {
+        AWSKMS kmsClient = AWSKMSClientBuilder.standard().withRegion("us-east-1").build();
+        Path path = Paths.get("/Users/adi/Downloads/ec.txt");
+        byte[] bytes = Files.readAllBytes(path);
+        ByteBuffer buffer = ByteBuffer.wrap(bytes);
+        DecryptRequest decryptRequest = new DecryptRequest();
+        decryptRequest.setCiphertextBlob(buffer);
+        DecryptResult decryptResult = kmsClient.decrypt(decryptRequest);
+        System.out.println("DecryptedText" + new String(decryptResult.getPlaintext().array()));
+
+        System.out.println("Hello World!");
+    }
+}
